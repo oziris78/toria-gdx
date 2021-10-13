ae95244af8772435e5efd1588a9a090e7e29d562
OziRis78 <oguzhantg3@gmail.com>
2021-10-13 16:27:54 +0300

    fixing imports for v0.0.5

--- src/test/java/com/telek/telekgdx/Main.java
+++ src/test/java/com/telek/telekgdx/Main.java
@@ -1,16 +1,16 @@
 package com.telek.telekgdx;
 
-//import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-//import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.telek.telekgdx.tests.TextureTest;
 
 public class Main {
 
 
     public static void main(String[] args) {
-//        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//        config.width = 600;  config.height = 600;
-//        new LwjglApplication(new TextureTest(), config);
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 600;  config.height = 600;
+        new LwjglApplication(new TextureTest(), config);
     }
 
 }
